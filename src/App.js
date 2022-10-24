38afc64389bd0fc3ebcfa54d0f1921e4afeb0068
Oğuzhan Kalkuz <kalkuzoguzhann@gmail.com>
2022-10-24 13:01:12 +0300

    7.7

--- src/App.js
+++ src/App.js
@@ -1,63 +1,73 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const useField = (type) => {
-  const [value, setValue] = useState('')
+  const [value, setValue] = useState('');
 
   const onChange = (event) => {
-    setValue(event.target.value)
-  }
+    setValue(event.target.value);
+  };
 
   return {
     type,
     value,
-    onChange
-  }
-}
+    onChange,
+  };
+};
 
 const useCountry = (name) => {
-  const [country, setCountry] = useState(null)
+  const [country, setCountry] = useState(null);
 
-  useEffect(() => {})
+  useEffect(() => {
+    axios.get(`https://restcountries.com/v3.1/name/${name}?fullText=true`)
+      .then(({ data }) => setCountry(data[0] || null))
+      .catch((err) => setCountry(null));
+  }, [name]);
 
-  return country
-}
+  return country;
+};
 
-const Country = ({ country }) => {
+function Country({ country }) {
   if (!country) {
-    return <div>not found...</div>
+    return <div>not found...</div>;
   }
 
   return (
     <div>
       <h3>{country.name.common}</h3>
-      <div>population {country.population}</div> 
-      <div>capital {country.capital}</div>
-      <img src={country.flags.png} height='100' alt={`flag of ${country.name.common}`}/> 
+      <div>
+        population
+        {country.population}
+      </div>
+      <div>
+        capital
+        {country.capital}
+      </div>
+      <img src={country.flags.png} height="100" alt={`flag of ${country.name.common}`} />
     </div>
-  )  
+  );
 }
 
-const App = () => {
-  const nameInput = useField('text')
-  const [name, setName] = useState('')
-  const country = useCountry(name)
+function App() {
+  const nameInput = useField('text');
+  const [name, setName] = useState('');
+  const country = useCountry(name);
 
   const fetch = (e) => {
-    e.preventDefault()
-    setName(nameInput.value)
-  }
+    e.preventDefault();
+    setName(nameInput.value);
+  };
 
   return (
     <div>
       <form onSubmit={fetch}>
         <input {...nameInput} />
-        <button>find</button>
+        <button type="submit">find</button>
       </form>
 
       <Country country={country} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
